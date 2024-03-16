1eb79ce935c6a6ae178dcca44a6615caa1f9a0a3
ThirdWinter <zzx15601035637@outlook.com>
2024-03-16 13:53:07 +0800

    今日开发前暂存

--- utils/vaildator/vaildator.go
+++ utils/vaildator/vaildator.go
@@ -11,6 +11,13 @@ func ValidateUserRegistration(user models.User) (string, int) {
 		return "用户名长度限制4-12位", rspcode.ERROR
 	}
 
+	if len(user.Password) < 4 || len(user.Password) > 12 {
+		return "用户密码限制4-12位", rspcode.ERROR
+	}
+
+	if user.Role != 1 {
+		return "用户无法申请更高权限", rspcode.ERROR
+	}
 	// if err := validate.Var(user.Password, "required,min=6,max=20"); err != nil {
 	// 	return "密码长度限制6-20位",rspcode.ERROR
 	// }
